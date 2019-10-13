3ff0d95963a9b9222fdb8bc7b5bf555248b9b628
Kellojo <kellojo1@gmail.com>
2019-10-13 13:44:22 +0200

    Updated Packages, Added CORS To Backend

--- functions/app.js
+++ functions/app.js
@@ -1,12 +1,15 @@
 
 
 const express = require("express")
+const cors = require('cors')
 
 /* Routes */
 const usersRoute = require("./routes/Users")
 
 /* Initilize our app and the firebase admin sdk */
 var app = express();
+
+app.use(cors())
 
 /* Use our created routes */
 app.use("/users", usersRoute);
