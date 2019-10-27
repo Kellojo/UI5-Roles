7f53573c7dc05d79d7d18062441a4f6954b2a951
Kellojo <kellojo1@gmail.com>
2019-10-27 11:24:14 +0100

    Added Authorization Check Middleware To Protect All Requests

--- functions/app.js
+++ functions/app.js
@@ -1,7 +1,13 @@
 
 
 const express = require("express")
-const cors = require('cors')
+const cors = require('cors')();
+const cookieParser = require('cookie-parser')();
+
+
+
+const AuthController = require("./controller/Auth");
+
 
 /* Routes */
 const usersRoute = require("./routes/Users")
@@ -10,7 +16,10 @@ const rolesRoute = require("./routes/Roles")
 /* Initilize our app and the firebase admin sdk */
 var app = express();
 
-app.use(cors())
+/* Apply Middleware */
+app.use(cors);
+app.use(cookieParser);
+app.use(AuthController.validateFirebaseIdToken);
 
 /* Use our created routes */
 app.use("/users", usersRoute);
---
+++ functions/controller/Auth.js
@@ -0,0 +1,67 @@
+  
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+const admin = require('firebase-admin');
+
+const UNAUTHORIZED_ERROR_MESSAGE = "You are not authorized to perform this request.";
+
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+exports.validateFirebaseIdToken = async (req, res, next) => {
+    console.log('Check if request is authorized with Firebase ID token');
+
+    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+        !(req.cookies && req.cookies.__session)) {
+        console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+            'Make sure you authorize your request by providing the following HTTP header:',
+            'Authorization: Bearer <Firebase ID Token>',
+            'or by passing a "__session" cookie.');
+        res.status(403).send(UNAUTHORIZED_ERROR_MESSAGE);
+        return;
+    }
+
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        console.log('Found "Authorization" header');
+        // Read the ID Token from the Authorization header.
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else if (req.cookies) {
+        console.log('Found "__session" cookie');
+        // Read the ID Token from cookie.
+        idToken = req.cookies.__session;
+    } else {
+        // No cookie
+        res.status(403).send('You are not authorized to perform this request');
+        return;
+    }
+
+    try {
+        const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+        console.log('ID Token correctly decoded', decodedIdToken);
+        req.user = decodedIdToken;
+        next();
+        return;
+    } catch (error) {
+        console.error('Error while verifying Firebase ID token:', error);
+        res.status(403).send(UNAUTHORIZED_ERROR_MESSAGE);
+        return;
+    }
+};
